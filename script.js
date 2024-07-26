e3312a0500db53ed6fbdc8714bf41743ab1aff10
dev-carl-robert <dev.carl.robert@gmail.com>
2024-07-26 20:55:19 -0300

    banners automatico

---
+++ script.js
@@ -0,0 +1,15 @@
+let banners = document.querySelectorAll('.banner');
+    let currentBannerIndex = 0;
+
+    function alternarBanner() {
+      if (banners[currentBannerIndex]) {
+        banners[currentBannerIndex].classList.remove('selecionado');
+      }
+      currentBannerIndex = (currentBannerIndex + 1) % banners.length;
+      if (banners[currentBannerIndex]) {
+        banners[currentBannerIndex].classList.add('selecionado');
+    console.log("estou funcionando")
+    }
+    }
+
+    setInterval(alternarBanner, 6000);
